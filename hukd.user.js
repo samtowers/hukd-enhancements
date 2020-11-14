a64d99e80b880684d3151d97b23057eeee980da3
Sam Towers <jto.sam@gmail.com>
2020-11-14 01:25:49 +0000

    Change indent size from 2 -> 4

--- hukd.user.js
+++ hukd.user.js
@@ -9,19 +9,19 @@
 let customHTML = `
 <style>
 .cust-btn {
-  margin: 10px;
-  margin-right: 0px;
+    margin: 10px;
+    margin-right: 0px;
 }
 #batchSeparator {
-  font-size: 4em;
-  margin: 30px;
+    font-size: 4em;
+    margin: 30px;
 }
 </style>
 <button id="sortScore" class="btn--mode-special btn cust-btn">Sort Temp</button>
 <button id="sortPrice" class="btn--mode-special btn cust-btn">Sort Price</button>
-<button id="over200" class="btn--mode-special btn cust-btn">&gt;=200°</button>
-<button id="over300" class="btn--mode-special btn cust-btn">&gt;=300°</button>
-<button id="over500" class="btn--mode-special btn cust-btn">&gt;=500°</button>
+<button id="over200" class="btn--mode-special btn cust-btn">&gt;=200ï¿½</button>
+<button id="over300" class="btn--mode-special btn cust-btn">&gt;=300ï¿½</button>
+<button id="over500" class="btn--mode-special btn cust-btn">&gt;=500ï¿½</button>
 <button id="minPrice" class="btn--mode-special btn cust-btn">Min Price</button>
 <button id="maxPrice" class="btn--mode-special btn cust-btn">Max Price</button>
 `;
@@ -33,108 +33,108 @@ content.innerHTML = customHTML + content.innerHTML;
 
 document.querySelector('#sortScore').addEventListener('click', sortScore);
 document.querySelector('#sortPrice').addEventListener('click', sortPrice);
-document.querySelector('#over200').addEventListener( 'click', e => overTemp(200));
-document.querySelector('#over300').addEventListener( 'click', e => overTemp(300));
-document.querySelector('#over500').addEventListener( 'click', e => overTemp(500));
+document.querySelector('#over200').addEventListener('click', e => overTemp(200));
+document.querySelector('#over300').addEventListener('click', e => overTemp(300));
+document.querySelector('#over500').addEventListener('click', e => overTemp(500));
 document.querySelector('#minPrice').addEventListener('click', e => {
-  let min = promptFloat();
-  if (Number.isNaN(min)) {
-    return;
-  }
-  alterDeals(deals => {
-    return deals.filter(deal => deal.price >= min);
-  });
+    let min = promptFloat();
+    if (Number.isNaN(min)) {
+        return;
+    }
+    alterDeals(deals => {
+        return deals.filter(deal => deal.price >= min);
+    });
 });
 document.querySelector('#maxPrice').addEventListener('click', e => {
-  let max = promptFloat();
-  if (Number.isNaN(max)) {
-    return;
-  }
-  alterDeals(deals => {
-    return deals.filter(deal => deal.price <= max);
-  });
+    let max = promptFloat();
+    if (Number.isNaN(max)) {
+        return;
+    }
+    alterDeals(deals => {
+        return deals.filter(deal => deal.price <= max);
+    });
 });
 
 function promptFloat() {
-  return parseFloat(prompt());
+    return parseFloat(prompt());
 }
 
 function sortScore() {
-  alterDeals(deals => {
-    return deals.sort((a,b) => b.score - a.score); // Desc
-  });
+    alterDeals(deals => {
+        return deals.sort((a, b) => b.score - a.score); // Desc
+    });
 }
 function sortPrice() {
-  alterDeals(deals => {
-    console.log(deals);
-    return deals.sort((a,b) => a.price - b.price); // Asc
-  });
+    alterDeals(deals => {
+        console.log(deals);
+        return deals.sort((a, b) => a.price - b.price); // Asc
+    });
 }
 function overTemp(temp) {
-  alterDeals(deals => {
-    return deals.filter(deal => deal.score >= temp);
-  });
+    alterDeals(deals => {
+        return deals.filter(deal => deal.score >= temp);
+    });
 }
-
-
 
 /**
  * Update the DOM and alter deals as per callback.
  */
 function alterDeals(alterCB) {
-  let dealParent = null;
-  
-  let deals = Array.from(document.querySelectorAll('.thread--deal')).map(function(dealElem){
-    let deal = scrapeDealElem(dealElem);
-    if (!dealParent) {
-      dealParent = dealElem.parentElement;
-    }
-    dealElem.remove();
-    deal.elem = dealElem;
-    return deal;
-  });
+    let dealParent = null;
 
-  if (deals.length < 1) {
-    return;
-  }
-  
-  let updatedDeals = alterCB(deals);
-  
-  // Delete old batch separator, if exists:
-  let bs = document.querySelector('#batchSeparator');
-  if (bs) {
-    bs.remove();
-  }
-  // Update DOM:
-  let updatedDealsHTML = updatedDeals.map(function(deal){return deal.elem.outerHTML;}).join('');
-  //let dealLocationMarker = document.querySelector('section.thread--type-list');
-  dealParent.innerHTML = dealParent.innerHTML + updatedDealsHTML + '<div id="batchSeparator">Batch completed.</div>';
-  
-  //dealLocationMarker.outerHTML += updatedDealsHTML + '<div id="batchSeparator">Batch completed.</div>';
+    let deals = Array.from(document.querySelectorAll('.thread--deal')).map(function (dealElem) {
+        let deal = scrapeDealElem(dealElem);
+        if (!dealParent) {
+            dealParent = dealElem.parentElement;
+        }
+        dealElem.remove();
+        deal.elem = dealElem;
+        return deal;
+    });
+
+    if (deals.length < 1) {
+        return;
+    }
+
+    let updatedDeals = alterCB(deals);
+
+    // Delete old batch separator, if exists:
+    let bs = document.querySelector('#batchSeparator');
+    if (bs) {
+        bs.remove();
+    }
+    // Update DOM:
+    let updatedDealsHTML = updatedDeals.map(function (deal) { return deal.elem.outerHTML; }).join('');
+    dealParent.innerHTML = dealParent.innerHTML + updatedDealsHTML + '<div id="batchSeparator">Batch completed.</div>';
 }
 
 /**
  * Parse price, score etc from a deal element.
  */
 function scrapeDealElem(elem) {
-  // Parse Score:
-  let score = 0;
-  let voteElem = elem.querySelector('.vote-box');
-  if (voteElem) {
-    score = parseInt(voteElem.innerText);
-  }
+    // Parse Score:
+    let score = 0;
+    let voteElem = elem.querySelector('.vote-box');
+    if (voteElem) {
+        score = parseInt(voteElem.innerText);
+    }
 
-  // Parse Price:
-  let price = 0;
-  let priceElem = elem.querySelector('.thread-price');
+    // Parse Price:
+    let price = 0;
+    let priceElem = elem.querySelector('.thread-price');
 
-  if (priceElem) {
-    // NB: parseInt cannot handle the £ prefix nor commas.
-    price = parseInt(priceElem.innerText.replace('£', '').replace(',', ''));
-  }
+    if (priceElem) {
+        // NB: parseInt cannot handle the ï¿½ prefix nor commas.
+        price = parseInt(priceElem.innerText.replace('ï¿½', '').replace(',', ''));
+    }
 
-  return {
-    score: score,
-    price: price,
-  };
+    return {
+        score: score,
+        price: price,
+    };
+}
+
+function loadPage() {
+    console.log('load');
+
 }
